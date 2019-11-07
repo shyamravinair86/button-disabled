51e881b2c0e5466e3a28edc5659a9b86fbef6d00
shyamravinair86 <shyam.ravinair@gmail.com>
2019-11-07 21:05:47 +0530

    changes in button

--- force-app/main/default/lwc/buttonDisableCmp/buttonDisableCmp.js
+++ force-app/main/default/lwc/buttonDisableCmp/buttonDisableCmp.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class buttonDisableCmp extends LightningElement {}
+export default class buttonDisableCmp extends LightningElement {
+    renderedCallback() {
+        let test = this.template.querySelector('lightning-button');
+        test.disabled = true;
+    }
+}
